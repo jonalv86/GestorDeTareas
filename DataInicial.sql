063461a473f6588490268acc5f6e7fdcb1ef502b
Alvarez, Jonathan (J.) <JALVA172@ford.com>
2019-05-18 13:59:55 -0300

    Se crea el insert de datos hardcodeados

---
+++ DataInicial.sql
@@ -0,0 +1,8 @@
+﻿-- Estados
+INSERT INTO EstadoSet VALUES ('Pendiente'),('Iniciado'),('Terminado');
+
+-- Prioridades
+INSERT INTO PrioridadSet VALUES ('Baja'),('Media'),('Alta');
+
+-- Usuarios
+INSERT INTO UsuarioSet VALUES ('Juan'),('Miguel'),('Ana');
